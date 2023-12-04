b000b9b8835f058b8399fe1068f96faaaee86f6c
salEZY <bobicaleks@yahoo.com>
2023-12-04 22:40:19 +0100

    install dependencies, setup basic routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,25 @@
-
+import {Suspense, lazy} from 'react'
+import {CircularProgress} from '@mui/material'
+import { Routes, Route } from "react-router-dom";
 import './App.css'
+
+// Lazy loaded components
+const Home = lazy(() => import('./components/Home'))
+const Schedule = lazy(() => import('./components/Schedule'))
+const Shows = lazy(() => import('./components/Shows'))
 
 function App() {
 
   return (
-    <div>
-      FIDIJA
+    <Suspense fallback={<CircularProgress />}>
+    <div className='root'>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/schedule' element={<Schedule />} />
+        <Route path='/shows' element={<Shows />} />
+      </Routes>
     </div>
+    </Suspense>
   )
 }
 
